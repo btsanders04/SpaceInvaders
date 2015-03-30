d66709a1b407b59ff158c96d7a24c63a84a60f07
originalnumber2 <adamjarrell@Adams-MacBook-Pro-4.local>
2015-03-30 13:18:56 -0400

    Controller changes

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,28 +1,41 @@
 #include <Bounce2.h>
 
 int firePin = 9;
-Bounce bouncer;
+int reloadPin = 8;
+Bounce bouncer1;
+Bounce bouncer2;
 
 void setup () {
-  bouncer = Bounce();
+  bouncer1 = Bounce();
+  bouncer2 = Bounce();
   pinMode(firePin, INPUT);
-  bouncer .attach( firePin );
-  bouncer .interval(5);
+  pinMode(reloadPin, INPUT);
+  bouncer1 .attach( firePin );
+  bouncer2 .attach( reloadPin );
+  bouncer1 .interval(5);
+  bouncer2 .interval(5);
+
   Serial.begin(9600);
   Serial.println('Y');
 }
 
 
 void loop () {
- // if(Serial.available()>0) {
-   int pos = analogRead(A0);
-   Serial.print(pos);
-   Serial.print(",");
-   delay(1);
-   bouncer.update();
-   int fire = bouncer.read();
-   Serial.println(fire);  
-//  }
+  // if(Serial.available()>0) {
+  int pos = analogRead(A0);
+  Serial.print(pos);
+  Serial.print(",");
+  delay(1);
+  bouncer1.update();
+  int fire = bouncer1.read();
+  Serial.print(fire); 
+  Serial.print(",");
+  delay(1);
+  bouncer2.update();
+  int reload = bouncer2.read();
+  Serial.println(reload);
+  //  }
 }
-  
-  
+
+
+
