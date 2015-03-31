ae9ae2f78274abe4de92be8052bac81617009d93
originalnumber2 <adamjarrell@Adams-MacBook-Pro-4.local>
2015-03-30 20:25:49 -0400

    sounds added!

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -2,6 +2,7 @@
 
 int firePin = 7;
 int reloadPin = 5;
+int speakerPin = 10;
 //Bounce bouncer1;
 //Bounce bouncer2;
 
@@ -10,7 +11,8 @@ void setup () {
   //bouncer2 = Bounce();
   pinMode(firePin, INPUT);
   pinMode(reloadPin, INPUT);
- // bouncer1 .attach( firePin );
+  pinMode(speakerPin, OUTPUT);
+  // bouncer1 .attach( firePin );
   //bouncer2 .attach( reloadPin );
   //bouncer1 .interval(10);
   //bouncer2 .interval(10);
@@ -36,10 +38,42 @@ void loop () {
   Serial.print(",");
   delay(1);
   Serial.println(reload);
+  if(fire == 1){
+    playNote('c', 30);
+    playNote('d', 30);
+    playNote('e', 30);
+    playNote('f', 30);
+  }
   
-  fire = 0;
-  reload = 0;
-  //  }
+  if(reload == 1){
+    playNote('C', 30);
+    playNote('b', 30);
+    playNote('a', 30);
+    playNote('g', 30);
+  }
+    //  }
+}
+
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
+}
+
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(tone);
+
+  }
 }
 
 
