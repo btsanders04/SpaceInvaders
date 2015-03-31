2c78e2193b29a3e46920cfb1fa8bcdaa25c8bc52
originalnumber2 <adamjarrell@Adams-MacBook-Pro-4.local>
2015-03-30 21:09:37 -0400

    dreaming of good wires

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -3,19 +3,17 @@
 int firePin = 7;
 int reloadPin = 5;
 int speakerPin = 10;
-//Bounce bouncer1;
-//Bounce bouncer2;
+Bounce bouncer1 = Bounce();
+Bounce bouncer2 = Bounce();
 
 void setup () {
-  //bouncer1 = Bounce();
-  //bouncer2 = Bounce();
   pinMode(firePin, INPUT);
   pinMode(reloadPin, INPUT);
   pinMode(speakerPin, OUTPUT);
-  // bouncer1 .attach( firePin );
-  //bouncer2 .attach( reloadPin );
-  //bouncer1 .interval(10);
-  //bouncer2 .interval(10);
+  bouncer1 .attach( firePin );
+  bouncer2 .attach( reloadPin );
+  bouncer1 .interval(10);
+  bouncer2 .interval(10);
 
   Serial.begin(9600);
   Serial.println('Y');
@@ -25,12 +23,12 @@ void setup () {
 void loop () {
   // if(Serial.available()>0) {
   int pos = analogRead(A0);
-  //bouncer1.update();
-  //int fire = bouncer1.rose();
-  int fire = digitalRead(firePin);
-   //bouncer2.update();
-  //int reload = bouncer2.rose();
-  int reload = digitalRead(reloadPin);
+  bouncer1.update();
+  int fire = bouncer1.fell();
+  //int fire = digitalRead(firePin);
+  bouncer2.update();
+  int reload = bouncer2.fell();
+  //int reload = digitalRead(reloadPin);
   Serial.print(pos);
   Serial.print(",");
   delay(1);
@@ -44,20 +42,22 @@ void loop () {
     playNote('e', 30);
     playNote('f', 30);
   }
-  
+
   if(reload == 1){
     playNote('C', 30);
     playNote('b', 30);
     playNote('a', 30);
     playNote('g', 30);
   }
-    //  }
+  //  }
 }
 
 void playNote(char note, int duration) {
-  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
-  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
-  
+  char names[] = { 
+    'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C'   };
+  int tones[] = { 
+    1915, 1700, 1519, 1432, 1275, 1136, 1014, 956   };
+
   // play the tone corresponding to the note name
   for (int i = 0; i < 8; i++) {
     if (names[i] == note) {
@@ -75,6 +75,7 @@ void playTone(int tone, int duration) {
 
   }
 }
+
 
 
 
